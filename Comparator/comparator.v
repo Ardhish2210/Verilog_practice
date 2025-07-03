00d780749a9ea77e9770f26ca2e15087b3e02b52
Ardhish Patel <ardhishp@gmail.com>
2025-07-03 11:38:42 +0530

    comparator.v

---
+++ Comparator/comparator.v
@@ -0,0 +1,26 @@
+module comparator(a, b, LT, GT, EQ);
+
+input [3:0] a, b;
+output reg LT, GT, EQ;
+integer i;
+
+always @(*) begin
+    for (i=0; i<4; i=i+1) begin
+        if (a[i] > b[i]) begin
+            GT = 1'b1;
+            LT = 1'b0;
+            EQ = 1'b0;
+        end else if (a[i] < b[i]) begin
+            GT = 1'b0;
+            LT = 1'b1;
+            EQ = 1'b0;
+        end
+    end
+  
+    if(a == b) begin
+        GT = 1'b0;
+        LT = 1'b0;
+        EQ = 1'b1;
+    end
+end
+endmodule
