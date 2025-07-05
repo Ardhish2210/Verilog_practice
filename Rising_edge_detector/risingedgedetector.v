475844993b126a928bb666d9e6070b2f5cf21caf
Ardhish Patel <ardhishp@gmail.com>
2025-07-05 17:07:07 +0530

    risingedgedetector.v

---
+++ Rising_edge_detector/risingedgedetector.v
@@ -0,0 +1,19 @@
+module risingedgedetector (clk, d, rst, out);
+  
+    reg q;
+    input clk;
+    input d; 
+    input rst;
+    output reg out;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            q   <= 1'b0;
+            out <= 1'b0;
+        end else begin
+            out <= d & ~q;  
+            q   <= d;       
+        end
+    end
+  
+endmodule
