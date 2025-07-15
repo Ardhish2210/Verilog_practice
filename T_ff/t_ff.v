29cba773e2fed7920462611faec650dd5d6a5248
Ardhish Patel <ardhishp@gmail.com>
2025-07-15 21:04:15 +0530

    t_ff.v

---
+++ T_ff/t_ff.v
@@ -0,0 +1,16 @@
+module t_ff (t, clk, rst, q);
+
+input t, clk, rst;
+output reg q;
+
+always @(posedge clk or posedge rst) begin
+    if(rst) begin
+        q <= 1'b0;
+    end else begin
+        case (t)
+        1'b0: q <= q;
+        1'b1: q <= ~q;
+    endcase
+    end
+end 
+endmodule
