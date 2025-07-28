59711cd6bc9c1a1f38c58ed65879b2006867d679
Ardhish Patel <ardhishp@gmail.com>
2025-07-28 18:54:35 +0530

    Declared the inputs and outputs with their significance

--- Mood_based_RGB_controller/mood.v
+++ Mood_based_RGB_controller/mood.v
@@ -1,7 +1,7 @@
-module mood #(
-    parameters
-) (
-    ports
-);
-    
+module mood (brain_wave, clk, rst, rgb, blink);
+
+input [1:0] brain_wave; // 00-calm, 01-focused, 10-anxious, 11-excited
+input clk, rst;
+output [2:0] rgb; // rgb[0]=Blue, rgb[1]=Green, rgb[2]=Red,
+output blink; // blink = 1 (if ANXIOUS or EXCITED mode is ON)
 endmodule
