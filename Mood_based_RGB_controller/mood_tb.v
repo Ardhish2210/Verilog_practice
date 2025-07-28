8712f13402d8f73f9c0cac778fb5e4541f996b55
Ardhish Patel <ardhishp@gmail.com>
2025-07-28 20:25:19 +0530

    Written the corrected tb and generated "mood.vcd" for the waveform analysis"

--- Mood_based_RGB_controller/mood_tb.v
+++ Mood_based_RGB_controller/mood_tb.v
@@ -1,3 +1,6 @@
+`timescale 1ns/1ns
+`include "mood.v"
+
 module mood_tb; 
 
 reg [1:0] brain_wave;
@@ -7,6 +10,26 @@ wire blink;
 
 mood uut (brain_wave, clk, rst, rgb, blink);
 
+initial begin
+    $dumpfile("mood.vcd");
+    $dumpvars(0, mood_tb);
 
+    $monitor("Time: %0t || rst: %0b || brain_wave: %02b || rgb: %03b || blink: %0b", $time, rst, brain_wave, rgb, blink);
+
+    clk = 0;
+    rst = 1;
+    brain_wave = 0;
+
+    #5 rst = 0;
+    #8 brain_wave = 2'b00;
+    #10 brain_wave = 2'b00;
+    #10 brain_wave = 2'b01;
+    #10 brain_wave = 2'b10;
+    #10 brain_wave = 2'b11;
+
+    #10 $finish;
+end
+
+always #5 clk = ~clk;
     
 endmodule
