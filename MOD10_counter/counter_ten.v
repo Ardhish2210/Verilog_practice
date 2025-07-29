af198831b638e6cc5f936607d0a2a20772a70aac
Ardhish Patel <ardhishp@gmail.com>
2025-07-29 17:33:49 +0530

    Corrected the counter bits

--- MOD10_counter/counter_ten.v
+++ MOD10_counter/counter_ten.v
@@ -1,10 +1,12 @@
 module counter_ten (clk, rst, counter);
 
 input clk, rst;
-output counter;
+output [3:0] counter;
 
 always @(posedge clk or posedge rst) begin
     if(rst) begin
+        counter <= 4'b0000;
+    end else if (counter == 4'b1001) begin
         
     end
 
