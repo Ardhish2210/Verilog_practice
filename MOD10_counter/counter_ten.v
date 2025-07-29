3cb15fae24316196d587b1297baba1b88b99be64
Ardhish Patel <ardhishp@gmail.com>
2025-07-29 17:32:11 +0530

    Always block created

--- MOD10_counter/counter_ten.v
+++ MOD10_counter/counter_ten.v
@@ -3,5 +3,13 @@ module counter_ten (clk, rst, counter);
 input clk, rst;
 output counter;
 
+always @(posedge clk or posedge rst) begin
+    if(rst) begin
+        
+    end
+
+end
+
+
     
 endmodule
