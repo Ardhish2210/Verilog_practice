038731a3cf4f7ecfaf27558129bf5c15407034b3
Ardhish Patel <ardhishp@gmail.com>
2025-07-29 17:45:08 +0530

    .vcd file generated

--- MOD10_counter/counter_ten_tb.v
+++ MOD10_counter/counter_ten_tb.v
@@ -10,7 +10,17 @@ counter_ten uut (clk, rst, counter);
 
 initial begin 
     $dumpfile("counter_ten.vcd");
-    $dumpvars(0, counter_ten_tb.v);
-    
+    $dumpvars(0, counter_ten_tb);
+
+    $monitor("Time: %0t || clk: %0b || rst: %0b || counter: %0b", $time, clk, rst, counter);
+
+    clk = 0;
+    rst = 1;
+
+    #3 rst = 0;
+    #40 rst = 1;
+
+    #10 $finish;
+
 end
 endmodule
