82cbe0ce080b6d14d4814e4f4c476942bf7aae93
Ardhish Patel <ardhishp@gmail.com>
2025-07-29 17:38:46 +0530

    added code for creating ".vcd" file for output

--- MOD10_counter/counter_ten_tb.v
+++ MOD10_counter/counter_ten_tb.v
@@ -6,4 +6,11 @@ module counter_ten_tb;
 reg clk, rst;
 wire [3:0] counter;
 
+counter_ten uut (clk, rst, counter);
+
+initial begin 
+    $dumpfile("counter_ten.vcd");
+    $dumpvars(0, counter_ten_tb.v);
+    
+end
 endmodule
