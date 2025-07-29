10de33a3b6b4210010835aee2a5670afe9a96b6d
Ardhish Patel <ardhishp@gmail.com>
2025-07-29 17:35:32 +0530

    Corrected the overall code and wrote the logic

--- MOD10_counter/counter_ten.v
+++ MOD10_counter/counter_ten.v
@@ -1,17 +1,15 @@
 module counter_ten (clk, rst, counter);
 
 input clk, rst;
-output [3:0] counter;
+output reg [3:0] counter;
 
 always @(posedge clk or posedge rst) begin
     if(rst) begin
         counter <= 4'b0000;
     end else if (counter == 4'b1001) begin
-        
+        counter <= 4'b0000;
+    end else begin
+        counter <= counter + 1;
     end
-
 end
-
-
-    
 endmodule
