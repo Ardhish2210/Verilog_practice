190a98e276ca95495454a3e86d2890fd55362776
Ardhish Patel <ardhishp@gmail.com>
2025-08-01 12:39:11 +0530

    proper indentation

--- Seven_segement_display/seven_segement.v
+++ Seven_segement_display/seven_segement.v
@@ -16,18 +16,18 @@ always @(posedge clk or posedge rst) begin
         dp <= 1'b1;
         en <= 1'b0;
     end else begin
-    case (bin_in)
-    4'b0000: seg <= 7'b1111110;
-    4'b0001: seg <= 7'b0110000;
-    4'b0010: seg <= 7'b1101101;
-    4'b0011: seg <= 7'b1111001;
-    4'b0100: seg <= 7'b0110011;
-    4'b0101: seg <= 7'b1011011;
-    4'b0110: seg <= 7'b1011111;
-    4'b0111: seg <= 7'b1110000;
-    4'b1000: seg <= 7'b1111111;
-    4'b1001: seg <= 7'b1111011;
-    endcase
+        case (bin_in)
+            4'b0000: seg <= 7'b1111110;
+            4'b0001: seg <= 7'b0110000;
+            4'b0010: seg <= 7'b1101101;
+            4'b0011: seg <= 7'b1111001;
+            4'b0100: seg <= 7'b0110011;
+            4'b0101: seg <= 7'b1011011;
+            4'b0110: seg <= 7'b1011111;
+            4'b0111: seg <= 7'b1110000;
+            4'b1000: seg <= 7'b1111111;
+            4'b1001: seg <= 7'b1111011;
+        endcase
     end
 
     if (dp_en == 1'b1) begin
