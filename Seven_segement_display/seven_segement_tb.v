12a45018b240002e605552e0c3f78370681d7005
Ardhish Patel <ardhishp@gmail.com>
2025-08-01 12:03:45 +0530

    "$monitor" statment written

--- Seven_segement_display/seven_segement_tb.v
+++ Seven_segement_display/seven_segement_tb.v
@@ -12,5 +12,7 @@ seven_segement uut (clk, rst, bin_in, dp_en, blink_in, blink_rate, seg, dp, en);
 initial begin
     $dumpfile("seven_segement.vcd");
     $dumpvars(0, seven_segement_tb);
+
+    $monitor("Time: %0t || clk: %0b || rst: %0b || bin_in: %04b || dp_en: %0b || blink_en: %0b || seg: %07b || dp: %0b || en: %0b", $time, clk, rst, bin_in, dp_en, blink_en, seg, dp, en);
 end
 endmodule 
