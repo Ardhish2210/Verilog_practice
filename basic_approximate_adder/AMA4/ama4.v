b2bfddff65f6fd5385d9d5894994d1213bd97131
Ardhish Patel <ardhishp@gmail.com>
2025-08-02 12:20:36 +0530

    inputs and outputs declared + logic written

--- basic_approximate_adder/AMA4/ama4.v
+++ basic_approximate_adder/AMA4/ama4.v
@@ -1,7 +1,9 @@
-module ama4 #(
-    parameters
-) (
-    ports
-);
+module ama4 (a, b, cin, sum, cout);
+
+input a, b, cin;
+output sum, cout;
+
+assign sum = a ^ b ^cin;
+assign cout = a & b;
     
 endmodule
