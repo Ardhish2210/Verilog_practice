c70ee0950ebc903aa9a14b7b3a0d8860d5079ce4
Ardhish Patel <ardhishp@gmail.com>
2025-08-02 12:23:08 +0530

    tb code written and test vectors provided

---
+++ basic_approximate_adder/AMA4/ama4_tb.v
@@ -0,0 +1,29 @@
+`timescale 1ns/1ns
+`include "ama4.v"
+
+module approx_adder_tb; 
+
+reg a, b, cin;
+wire sum, cout;
+
+approx_adder uut (a, b, cin, sum, cout);
+
+initial begin
+    $dumpfile("ama4.vcd");
+    $dumpvars(0, ama4_tb);
+
+    $monitor("time: %0t || a: %0b || b: %0b || cin: %0b || sum: %0b || cout: %0b", $time, a, b, cin, sum, cout);
+
+    a = 1'b0;
+    b = 1'b0;
+    cin = 1'b0;
+
+    #3 a = 0; b = 0; cin = 1;
+    #10 a = 0; b = 1; cin = 0;
+    #10 a = 1; b = 0; cin = 1;
+    #10 a = 1; b = 1; cin = 0;
+
+    #10 $finish;
+end
+    
+endmodule
