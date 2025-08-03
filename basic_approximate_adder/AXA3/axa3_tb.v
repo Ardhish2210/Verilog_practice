11025002e9cdeb1cd1e542db3c9092d1bdda4ac5
Ardhish Patel <ardhishp@gmail.com>
2025-08-03 20:06:07 +0530

    Wrote the test vectors

--- basic_approximate_adder/AXA3/axa3_tb.v
+++ basic_approximate_adder/AXA3/axa3_tb.v
@@ -8,6 +8,21 @@ axa3 uut (a, b, cin, sum, cout);
 initial begin 
     $dumpfile("axa3.vcd");
     $dumpvars(0, axa3_tb);
+
+    $monitor("Time: %0t || a: %0b || b: %0b || cin: %0b || sum: %0b || cout: %0b", $time, a, b, cin, sum, cout);
+
+    a = 0;
+    b = 0;
+    cin = 0;
+
+    #10 a = 0; b = 0; cin = 0;
+    #10 a = 0; b = 0; cin = 1;
+    #10 a = 0; b = 1; cin = 0;
+    #10 a = 1; b = 1; cin = 0;
+    #10 a = 1; b = 0; cin = 0;
+
+    #10 $finish;
+
 end
     
 endmodule
