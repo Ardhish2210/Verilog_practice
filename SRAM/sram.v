3f1b7196a7bd80246bab7ea22803a1a534920671
Ardhish Patel <ardhishp@gmail.com>
2025-08-04 11:26:01 +0530

    updated the read and write logic

--- SRAM/sram.v
+++ SRAM/sram.v
@@ -1,7 +1,7 @@
 module sram (clk, w_enable, addr, data_in, data_out);
 
 input clk;
-input w_enable;
+input w_enable; // write enable
 input [7:0] addr, data_in;
 output [7:0] data_out;
 
@@ -9,5 +9,12 @@ output [7:0] data_out;
 reg [7:0] mem [0: 255];
 
 always @(posedge clk) begin
+    if(w_enable == 1'b1) begin
+        mem[addr] <= data_in; 
+    end 
+    else begin
+        data_out <= mem[addr];
+    end
+end
     
 endmodule
