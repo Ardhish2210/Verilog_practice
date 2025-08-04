25710873558856dc6128be15dc3d4f79f77eea52
Ardhish Patel <ardhishp@gmail.com>
2025-08-04 10:32:33 +0530

    Re-declared COUT

--- Carry_save_adder/carry_save_adder.v
+++ Carry_save_adder/carry_save_adder.v
@@ -1,9 +1,10 @@
 module carry_save_adder (A, B, C, SUM, COUT);
 
 input [3:0] A, B, C;
-output [3:0] SUM, COUT;
+output [3:0] SUM
+output COUT;
 
-full_adder F1 (.a(), .b(), .cin(), .sum(), .cout());
+full_adder F1 (.a(A[0]), .b(B[0]), .cin(C[0]), .sum(SUM[0]), .cout(COUT[0]));
 full_adder F2 (.a(), .b(), .cin(), .sum(), .cout());
 full_adder F3 (.a(), .b(), .cin(), .sum(), .cout());
 full_adder F4 (.a(), .b(), .cin(), .sum(), .cout());
