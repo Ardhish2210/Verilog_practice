fb1dc7bd069e31d4927d3fe92e8480e76b4b0260
Ardhish Patel <ardhishp@gmail.com>
2025-08-04 10:38:19 +0530

    ".vcd" file code written

--- Carry_save_adder/carry_save_adder_tb.v
+++ Carry_save_adder/carry_save_adder_tb.v
@@ -4,4 +4,10 @@ reg [3:0] A, B, C;
 wire [3:0] SUM, COUT;
 
 carry_save_adder uut (A, B, C, SUM, COUT);
+
+initial begin
+  $dumpfile("carry_save_adder.vcd");
+  $dumpvars(0, carry_save_adder_tb);
+
+end
 endmodule
