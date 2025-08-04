1cf79e25a0d5ce824bb53497e47d99bf097acc04
Ardhish Patel <ardhishp@gmail.com>
2025-08-04 11:29:03 +0530

    ".vcd" file code written

--- SRAM/sram_tb.v
+++ SRAM/sram_tb.v
@@ -4,5 +4,12 @@ reg clk;
 reg w_enable; 
 reg [7:0] addr, data_in;
 wire [7:0] data_out;
+
+sram uut (clk, w_enable, addr, data_in, data_out);
+
+initial begin 
+    $dumpfile("sram.vcd");
+    $dumpvars(0, sram_tb);
+end
     
 endmodule
