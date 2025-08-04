0e6ad058fcc75893df92951098b8b9e0a1f085a9
Ardhish Patel <ardhishp@gmail.com>
2025-08-04 11:34:57 +0530

    Test vectors given

--- SRAM/sram_tb.v
+++ SRAM/sram_tb.v
@@ -12,6 +12,18 @@ initial begin
     $dumpvars(0, sram_tb);
 
     $monitor("Time: %0t || clk: %0b || data_in: %07b || w_enable: %0b || data_out: %07b || addr: %07b", $time, clk, data_in, w_enable, data_out, addr);
+
+    clk = 0;
+    w_enable = 0;
+    addr = 7'b0000000;
+    data_in = 7'b0000000;
+
+    #10  w_enable = 0; addr = 7'b0000001; data_in = 7'b1111111;
+    #10  w_enable = 1; 
+    #10  w_enable = 0; addr = 7'b0000010; data_in = 7'b1111000;
+    #10  w_enable = 1;
+
+    #10 $finish;
 end
     
 endmodule
