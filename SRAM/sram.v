f7c0823bfcfe270cf362f10ad647a3ba85a820a2
Ardhish Patel <ardhishp@gmail.com>
2025-08-04 11:23:45 +0530

    "rst" removed

--- SRAM/sram.v
+++ SRAM/sram.v
@@ -1,11 +1,13 @@
-module sram (clk, rst, w_enable, addr, data_in, data_out);
+module sram (clk, w_enable, addr, data_in, data_out);
 
-input clk, rst;
+input clk;
 input w_enable;
 input [7:0] addr, data_in;
 output [7:0] data_out;
 
 // memory declared
 reg [7:0] mem [0: 255];
+
+always @(posedge clk) begin
     
 endmodule
