606a0676962ea64893f1d347dd942d491d7aff53
Ardhish Patel <ardhishp@gmail.com>
2025-08-04 10:30:09 +0530

    Instinciated the "full_adder" module in the main design module

--- Carry_save_adder/carry_save_adder.v
+++ Carry_save_adder/carry_save_adder.v
@@ -2,5 +2,10 @@ module carry_save_adder (A, B, C, SUM, COUT);
 
 input [3:0] A, B, C;
 output [3:0] SUM, COUT;
+
+full_adder F1 (.a(), .b(), .cin(), .sum(), .cout());
+full_adder F2 (.a(), .b(), .cin(), .sum(), .cout());
+full_adder F3 (.a(), .b(), .cin(), .sum(), .cout());
+full_adder F4 (.a(), .b(), .cin(), .sum(), .cout());
     
 endmodule   
