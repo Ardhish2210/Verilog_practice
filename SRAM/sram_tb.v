9c26600103686e8b9bb60d4a8d96bc2608ae746b
Ardhish Patel <ardhishp@gmail.com>
2025-08-04 11:31:42 +0530

    "monitor" statment added

--- SRAM/sram_tb.v
+++ SRAM/sram_tb.v
@@ -10,6 +10,8 @@ sram uut (clk, w_enable, addr, data_in, data_out);
 initial begin 
     $dumpfile("sram.vcd");
     $dumpvars(0, sram_tb);
+
+    $monitor("Time: %0t || clk: %0b || data_in: %07b || w_enable: %0b || data_out: %07b || addr: %07b", $time, clk, data_in, w_enable, data_out, addr);
 end
     
 endmodule
