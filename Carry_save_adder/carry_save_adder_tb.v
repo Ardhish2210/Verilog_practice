dfdd5075ddd247268ee78f016aba11d9aae7b3d6
Ardhish Patel <ardhishp@gmail.com>
2025-08-04 10:50:27 +0530

    Test vectors added

--- Carry_save_adder/carry_save_adder_tb.v
+++ Carry_save_adder/carry_save_adder_tb.v
@@ -11,5 +11,11 @@ initial begin
 
   $monitor("Time: %0t || A: %04b || B: %04b || C: %04b || SUM: %04b || COUT: %04b", $time, A, B, C, SUM, COUT);
 
+  A = 4'b0000; B = 4'b0000; C = 4'b0000;
+  #10 A = 4'b0000; B = 4'b0000; C = 4'b0000;
+  #10 A = 4'b0101; B = 4'b0011; C = 4'b0110;
+  #10 A = 4'b0010; B = 4'b1100; C = 4'b1010;
+
+  #10 $finish;
 end
 endmodule
