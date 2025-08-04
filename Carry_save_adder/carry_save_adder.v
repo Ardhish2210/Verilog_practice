2357694cbcc1196b9e0d975aab750fc22a17dfd4
Ardhish Patel <ardhishp@gmail.com>
2025-08-04 10:28:45 +0530

    inputs and outputs port declared

--- Carry_save_adder/carry_save_adder.v
+++ Carry_save_adder/carry_save_adder.v
@@ -1,7 +1,6 @@
-module carry_save_adder #(
-    parameters
-) (
-    ports
-);
+module carry_save_adder (A, B, C, SUM, COUT);
+
+input [3:0] A, B, C;
+output [3:0] SUM, COUT;
     
-endmodule
+endmodule   
