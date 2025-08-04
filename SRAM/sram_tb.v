09d49a957b182eb200f75ad815974089ecf0db27
Ardhish Patel <ardhishp@gmail.com>
2025-08-04 11:48:51 +0530

    Error resolved and Test vectors declared

--- SRAM/sram_tb.v
+++ SRAM/sram_tb.v
@@ -17,17 +17,19 @@ initial begin
     $monitor("Time: %0t || clk: %0b || data_in: %07b || w_enable: %0b || data_out: %07b || addr: %07b", $time, clk, data_in, w_enable, data_out, addr);
 
     clk = 0;
-    w_enable = 0;
-    addr = 7'b0000000;
-    data_in = 7'b0000000;
-
-    #10  w_enable = 0; addr = 7'b0000001; data_in = 7'b1111111;
-    #10  w_enable = 1; 
-    #10  w_enable = 0; addr = 7'b0000010; data_in = 7'b1111000;
-    #10  w_enable = 1;
+    w_enable = 1;
+    addr = 8'b00000000;
+    data_in = 8'b10101010;
+    
+    #10 w_enable = 1; addr = 8'b00000001; data_in = 8'b11001100;  
+    #10 w_enable = 1; addr = 8'b00000010; data_in = 8'b11110000;
+    #10 w_enable = 0;addr = 8'b00000000;  
+    #10 addr = 8'b00000001;  
+    #10 addr = 8'b00000010;
 
     #10 $finish;
 end
+
 always #5 clk = ~clk;
 
 endmodule
