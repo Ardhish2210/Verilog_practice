40efb2e460da1c0dbef0d129fa62c4cb0e5d902b
Ardhish Patel <ardhishp@gmail.com>
2025-08-05 19:25:08 +0530

    Test vectors declared

--- basic_approximate_adder/AMA2/ama2_tb.v
+++ basic_approximate_adder/AMA2/ama2_tb.v
@@ -12,7 +12,16 @@ initial begin
     $dumpfile("ama2.vcd");
     $dumpvars(0, ama2_tb);
 
-    
+    $monitor("a: %0b || b: %0b || cin: %0b || sum: %0b || cout: %0b", a, b, cin, sum, cout);
+
+    a = 0; b = 0; cin = 0;
+
+    #10 a = 0; b = 0; cin = 0;
+    #10 a = 1; b = 0; cin = 1;
+    #10 a = 1; b = 1; cin = 0;
+    #10 a = 0; b = 1; cin = 1;
+
+    #10 $finish;
 end
     
 endmodule
