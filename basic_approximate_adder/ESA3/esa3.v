db3f57c442a9eb2daed1303ae551c88e81cbedb9
Ardhish Patel <ardhishp@gmail.com>
2025-08-06 12:19:27 +0530

    Restarted the designing

---
+++ basic_approximate_adder/ESA3/esa3.v
@@ -0,0 +1,8 @@
+module esa3 (a, b, sum);
+
+input [7:0] a, b;
+output [8:0] sum;
+
+
+    
+endmodule
--- basic_approximate_adder/ESA3/ripple_adder.v
+++
@@ -1,8 +0,0 @@
-module ripple_adder (a, b, cin, sum, cout);
-
-input cin;
-input [3:0] a, b;
-output sum, cout;
-
-    
-endmodule
