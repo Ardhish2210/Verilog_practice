6680781a067fcb00b608f9e0ed4f8b27203a8e93
Ardhish Patel <ardhishp@gmail.com>
2025-08-06 12:31:23 +0530

    code for generating ".vcd" file added

--- basic_approximate_adder/ESA3/esa3_tb.v
+++ basic_approximate_adder/ESA3/esa3_tb.v
@@ -3,4 +3,19 @@ module esa3_tb;
 reg [7:0] a,b;
 wire [8:0] sum;
 
+esa3 uut (a, b, sum);
+
+initial begin
+    $dumpfile("esa3.vcd");
+    $dumpvars(0, esa3_tb);
+
+    $monitor("a: %08b || b: %08b || sum: %09b", a, b, sum);
+
+end
+
+
+
+
+
+
 endmodule
