74d40f806feb9428d21bdce70a3dc578ae7aa0d4
Ardhish Patel <ardhishp@gmail.com>
2025-08-08 02:33:32 +0530

    There was a logical error regarding the "lsb_carry",m which is now resolved and code is ready to run

--- basic_approximate_adder/SCSA/scsa.v
+++ basic_approximate_adder/SCSA/scsa.v
@@ -7,10 +7,14 @@ output [3:0] sum;
 output cout;
 
 wire c_1, c_2;
+wire [2:0] lsb_sum;
+wire lsb_carry;
 // the LSB's are added directly without any carry_in (approx)
-assign sum[1:0] = a[1:0] + b[1:0];
+assign lsb_sum = a[1:0] + b[1:0];
+assign sum[1:0] = lsb_sum[1:0];
+assign lsb_carry = lsb_sum [2];
 
-full_adder F1 (.a(a[2]), .b(b[2]), .cin(1'b0), .sum(sum[2]), .cout(c_1));
+full_adder F1 (.a(a[2]), .b(b[2]), .cin(lsb_carry), .sum(sum[2]), .cout(c_1));
 full_adder F2 (.a(a[3]), .b(b[3]), .cin(c_1), .sum(sum[3]), .cout(c_2));
 
 assign cout = c_2;
