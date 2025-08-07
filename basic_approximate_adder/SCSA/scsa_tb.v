505c7ec53a9c06444f210ddef468df79a9eef46c
Ardhish Patel <ardhishp@gmail.com>
2025-08-08 02:25:05 +0530

    Added "$monitor" statement

--- basic_approximate_adder/SCSA/scsa_tb.v
+++ basic_approximate_adder/SCSA/scsa_tb.v
@@ -10,13 +10,14 @@ initial begin
     $dumpfile("scsa.vcd");
     $dumpvars(0, scsa_tb);
 
+    $monitor("a: %04b || b: %04b || sum: %04b || cout: %0b", a, b, sum, cout);
+
     a = 4'b0000; b = 3'b0000;
     #10 a = 4'b0011; b = 4'b1101;
     #10 a = 4'b1111; b = 4'b1101;
     #10 a = 4'b1110; b = 4'b1001;
 
     #10 $finish;
-
 end
     
 endmodule
