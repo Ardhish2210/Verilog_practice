a0092603f3e78d0d3014ad34a4292f303ed85208
Ardhish Patel <ardhishp@gmail.com>
2025-08-08 02:11:03 +0530

    LSB logic written

--- basic_approximate_adder/SCSA/scsa.v
+++ basic_approximate_adder/SCSA/scsa.v
@@ -3,5 +3,8 @@ module scsa (a, b, sum, cout);
 input [3:0] a, b;
 output [3:0] sum;
 output cout;
+
+// the LSB's are added directly without any carry_in (approx)
+assign sum[1:0] = a[1:0] + b[1:0];
     
 endmodule
