a01bb009ea2a6d15bd35bcea403e482094891873
Ardhish Patel <ardhishp@gmail.com>
2025-08-08 11:49:25 +0530

    0th bit logic written

--- CLA_adder/cla.v
+++ CLA_adder/cla.v
@@ -1,10 +1,16 @@
-module cla (a, b, sum, cout);
+module cla (a, b, sum, cin,cout);
 
 input [3:0] a, b;
+input cin;
 output [3:0] sum;
 wire [3:0] P, G; // P = propagation carry, G = Generated carry
+wire [3:0] C;
 
-
+// for 0th bit
+assign P[0] = a[0] ^ b[0];
+assign G[0] = a[0] & b[0];
+assign C[0] = cin;
+assign sum[0] = P[0] ^ C[0];
 
 
 endmodule
