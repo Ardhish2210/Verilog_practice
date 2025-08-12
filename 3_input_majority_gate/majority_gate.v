d8e4b676d2dd1a2ca717b7d4352d63c3c716686c
Ardhish Patel <ardhishp@gmail.com>
2025-08-12 10:19:08 +0530

    A brief idea given about the design

--- 3_input_majority_gate/majority_gate.v
+++ 3_input_majority_gate/majority_gate.v
@@ -3,4 +3,5 @@ module majority_gate (a, b, c, out);
 input a, b, c;
 output out;
 
+//This is basically like VOTING MACHINE, The output will be 1 if there are majority 1's in the input and 0 if there are majority 0's in the input
 endmodule
