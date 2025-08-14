84ec0a34e2d1f369038b23dfcb4054afeb95cb69
Ardhish Patel <ardhishp@gmail.com>
2025-08-14 19:40:32 +0530

    Coded the full logic and solved certain errors

--- Tri_state_buffer/tri_state_buffer.v
+++ Tri_state_buffer/tri_state_buffer.v
@@ -1,19 +1,31 @@
-module (sel, enable, a, bus_line);
+module tri_state_buffer (sel, enable, a, bus_line);
 
 input [1:0] sel;
 input enable;
 input [3:0] a;
-output bus_line;
+output reg bus_line;
 
-wire [3:0] d;
+reg [3:0] d;
 
 always @(*) begin
     if (enable) begin
         case (sel)
-        2'b00: d[0] = 1'b1;
-        2'b01: d[1] = 1'b1;
-        2'b10: d[2] = 1'b1;
-        2'b11: d[3] = 1'b1;
+        2'b00: begin
+            d[0] = 1'b1;
+            bus_line = a[0];
+        end
+        2'b01: begin
+            d[1] = 1'b1;
+            bus_line = a[1];
+        end
+        2'b10: begin
+            d[2] = 1'b1;
+            bus_line = a[2];
+        end
+        2'b11: begin
+            d[3] = 1'b1;
+            bus_line = a[3];
+        end
         endcase
     end 
 end
