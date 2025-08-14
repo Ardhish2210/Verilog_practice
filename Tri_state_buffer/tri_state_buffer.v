0609d02eca0189cccbb243c762a3d12d63bd4d61
Ardhish Patel <ardhishp@gmail.com>
2025-08-14 19:35:09 +0530

    Written the 2x4 decoder logic

--- Tri_state_buffer/tri_state_buffer.v
+++ Tri_state_buffer/tri_state_buffer.v
@@ -5,4 +5,17 @@ input enable;
 input [3:0] a;
 output bus_line;
 
+wire [3:0] d;
+
+always @(*) begin
+    if (enable) begin
+        case (sel)
+        2'b00: d[0] = 1'b1;
+        2'b01: d[1] = 1'b1;
+        2'b10: d[2] = 1'b1;
+        2'b11: d[3] = 1'b1;
+        endcase
+    end 
+end
+
 endmodule
