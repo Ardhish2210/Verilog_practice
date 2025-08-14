ce37ca42cb424c06ff01a6a228868c5fe23eb657
Ardhish Patel <ardhishp@gmail.com>
2025-08-15 03:04:35 +0530

    Logic written but in a different approach

--- Priority_encoder/priority_encoder_new.v
+++ Priority_encoder/priority_encoder_new.v
@@ -2,7 +2,17 @@ module priority_encoder_new (d, out, valid);
     
 input valid;
 input [7:0] d;
-output [2:0] out;
+output reg [2:0] out;
+integer i;
 
+always @(*) begin
+    if(valid) begin
+        for(i = 0; i < 8; i = i + 1) begin
+            if(d[i] == 1'b1) begin
+                out = i;
+            end
+        end
+    end
+end
 
 endmodule
