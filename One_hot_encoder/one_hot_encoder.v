3267d6a029845744f445d7186094be7cd125ff09
Ardhish Patel <ardhishp@gmail.com>
2025-08-17 20:19:53 +0530

    Main logic coded and the main module is successfully run

--- One_hot_encoder/one_hot_encoder.v
+++ One_hot_encoder/one_hot_encoder.v
@@ -1,8 +1,19 @@
-module(a, out);
+module one_hot_encoder (a, out);
 
 input [3:0] a;
-output [7:0] out;
+output reg [7:0] out;
 
-
+always @(*) begin
+  case (a)
+    3'b000: out = 8'b0000_0001;
+    3'b001: out = 8'b0000_0010;
+    3'b010: out = 8'b0000_0100;
+    3'b011: out = 8'b0000_1000;
+    3'b100: out = 8'b0001_0000;
+    3'b101: out = 8'b0010_0000;
+    3'b110: out = 8'b0100_0000;
+    3'b111: out = 8'b1000_0000;
+  endcase
+end
 
 endmodule
