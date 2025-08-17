dd99f41a0e3d1854c57b67a57b7b360d4c64a3a5
Ardhish Patel <ardhishp@gmail.com>
2025-08-17 20:25:23 +0530

    Test vectors initialized

--- One_hot_encoder/one_hot_encoder_tb.v
+++ One_hot_encoder/one_hot_encoder_tb.v
@@ -4,4 +4,21 @@ reg [2:0] a;
 wire [7:0] out;
 
 one_hot_encoder uut (a, out);
+
+initial begin
+
+    $dumpfile("one_hot_encoder.vcd");
+    $dumpvars(0, one_hot_encoder_tb);
+
+    $monitor("a: %d || out: %08b", a, out);
+
+    a = 3'b000;
+
+    #10 a = 3'b001;
+    #10 a = 3'b010;
+    #10 a = 3'b100;
+    #10 a = 3'b111;
+
+    #10 $finish;
+end
 endmodule
