91da7dcbc4302ef5fadb0d3b3a215d9a2b496909
Ardhish Patel <ardhishp@gmail.com>
2025-08-18 19:53:52 +0530

    Test vectors written

--- 4_bit_FA/full_adder_tb.v
+++ 4_bit_FA/full_adder_tb.v
@@ -5,7 +5,23 @@ module full_adder_tb;
 
 reg a, b, cin;
 wire sum, cout;
+integer i;
 
 full_adder uut (a, b, cin, sum, cout);
+
+initial begin
+
+    $dumpfile("full_adder.vcd");
+    $dumpvars(0, full_adder_tb);
+
+    $monitor("a: %0b || b: %0b || cin: %0b || sum: %0b || cout: %0b", a, b, cin, sum, cout);
+
+    for (i = 0; i < 8; i = i + 1) begin
+        {a, b, cin} = i;
+        #5;
+    end
+
+    #10 $finish;
+end
 
 endmodule
