6d9f7c165ee13df2c80e3bc96c687a0572f282b8
Ardhish Patel <ardhishp@gmail.com>
2025-08-24 17:00:53 +0530

    Test vectors assigned

--- XOR_gate_using_NAND/nor_using_nand_tb.v
+++ XOR_gate_using_NAND/nor_using_nand_tb.v
@@ -12,5 +12,12 @@ initial begin
     $dumpvars(0, xor_using_nand_tb);
 
     $monitor("a: %b || b: %b || out: %b", a, b, out);
+
+    a = 1'b0; b = 1'b0; 
+    #10 a = 1'b0; b = 1'b1; 
+    #10 a = 1'b1; b = 1'b0;
+    #10 a = 1'b1; b = 1'b1;
+
+    #10 $finish;
 end
 endmodule
