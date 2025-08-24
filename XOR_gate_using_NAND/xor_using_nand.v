9d58abdf66c4ceaba4f11035e63b6cc120592213
Ardhish Patel <ardhishp@gmail.com>
2025-08-24 17:11:27 +0530

    Correct output recieved

--- XOR_gate_using_NAND/xor_using_nand.v
+++ XOR_gate_using_NAND/xor_using_nand.v
@@ -1,32 +1,13 @@
-$date
-	Sun Aug 24 17:04:28 2025
-$end
-$version
-	Icarus Verilog
-$end
-$timescale
-	1ns
-$end
-$scope module xor_using_nand_tb $end
-$var wire 1 ! y $end
-$var reg 1 " a $end
-$var reg 1 # b $end
-$upscope $end
-$enddefinitions $end
-$comment Show the parameter values. $end
-$dumpall
-$end
-#0
-$dumpvars
-0#
-0"
-z!
-$end
-#10
-1#
-#20
-0#
-1"
-#30
-1#
-#40
+module xor_using_nand (a, b, y);
+
+input a, b;
+output y;
+
+wire d, e, f;
+
+nand g1 (d, a, b);
+nand g2 (e, d, a);
+nand g3 (f, d, b);
+nand g4 (y, f, e);
+	
+endmodule
