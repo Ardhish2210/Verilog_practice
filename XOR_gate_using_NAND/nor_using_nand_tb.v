7db976f4473a68be9422120a4e4b92c3410e61b3
Ardhish Patel <ardhishp@gmail.com>
2025-08-24 16:59:30 +0530

    Wrote the code to get the ".vcd" file to see the waveform

--- XOR_gate_using_NAND/nor_using_nand_tb.v
+++ XOR_gate_using_NAND/nor_using_nand_tb.v
@@ -6,5 +6,11 @@ module xor_using_nand_tb;
 reg a, b;
 wire y;
 
+initial begin
 
+    $dumpfile("xor_using_nand.v");
+    $dumpvars(0, xor_using_nand_tb);
+
+    $monitor("a: %b || b: %b || out: %b", a, b, out);
+end
 endmodule
