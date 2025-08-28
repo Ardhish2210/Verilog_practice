01de9acc2b2535ffbbaf3bb8b5079f1cc8ddca9a
Ardhish Patel <ardhishp@gmail.com>
2025-08-28 12:56:12 +0530

    Main logic coded for the PWM signal

--- PWM_pulse_generator/pwm.v
+++ PWM_pulse_generator/pwm.v
@@ -4,5 +4,16 @@ input clk, rst;
 input [7:0] duty_cycle;
 output pwm_out;
 
+reg [7:0] counter;
+
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        counter <= 8'b0000_0000;
+    end else begin
+        counter <= counter + 1;  
+    end
+end
+
+assign pwm_out = (counter < duty_cycle) ? 1'b1:1'b0;
     
 endmodule
