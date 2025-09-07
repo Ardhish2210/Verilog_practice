b36ca7f5b02487810976ce923d2a5316a076d290
Ardhish Patel <ardhishp@gmail.com>
2025-09-07 15:10:42 +0530

    Changes made and got the correct output

--- SR_latch/sr_latch.v
+++ SR_latch/sr_latch.v
@@ -4,17 +4,14 @@ input s, r, clk, rst;
 output reg q;
 
 always @(posedge clk or posedge rst) begin
-    if(rst) begin
+    if (rst) begin
         q <= 0;
     end else begin
-        casex ({~s, ~r})
+        casex ({s, r})
         2'b00: q <= q;
         2'b01: q <= 1'b0;
         2'b10: q <= 1'b1;
         2'b11: q <= 1'bx;
-        default: begin
-            q <= 1'b0;
-        end
         endcase
     end
 end
