3d881c598e49dc49e520b9027969eccd355d2c6d
Ardhish Patel <ardhishp@gmail.com>
2025-09-07 20:14:34 +0530

    Coded the clock with the new and correct logic

--- 24_hours_clock/clock.v
+++ 24_hours_clock/clock.v
@@ -1,25 +1,37 @@
 module clock (clk, rst, hours, minutes, seconds);
 
 input clk, rst;
-output reg hours, minutes, seconds;
+output reg [4:0] hours; 
+output reg [5:0] minutes;
+output reg [5:0] seconds;
 
-integer count = 4999; // Because we r using a clock with frequency 5Khz, hence TimePeriod = 0.2ms and for seconds flag to be 1, 0.2 * 5000 = 1sec.
-integer i, j, k;
+reg [12:0] count; // Because we r using a clock with frequency 5Khz, hence TimePeriod = 0.2ms and for seconds flag to be 1, 0.2 * 5000 = 1sec.
 
-always @(posedge clk) begin
+always @(posedge clk or posedge rst) begin
     if (rst) begin
-        hours = 1'b0;
-        minutes = 1'b0;
-        seconds = 1'b0;
+        count   <= 0;
+        seconds <= 0;
+        minutes <= 0;
+        hours   <= 0;
     end else begin
-        for (i = 0; i <= count; i = i + 1) begin
-            seconds = 1'b1;
-            for (j = 0; j <= 59; j = j + 1) begin
-                minutes = 1'b1;
-                for (k = 0; k <= 59; k = k + 1) begin
-                    hours = 1'b1;
+        if (count == 4999) begin
+            count <= 0;
+            if (seconds == 59) begin
+                seconds <= 0;
+                if (minutes == 59) begin
+                    minutes <= 0;
+                    if (hours == 23)
+                        hours <= 0;
+                    else
+                        hours <= hours + 1;
+                end else begin
+                    minutes <= minutes + 1;
                 end
+            end else begin
+                seconds <= seconds + 1;
             end
+        end else begin
+            count <= count + 1;
         end
     end
 end
