59ed2ab02deac8be6774c53c1e8b319fa3c66f0f
Ardhish Patel <ardhishp@gmail.com>
2025-09-07 14:38:14 +0530

    Added a new method to parity checker

--- Parity_checker/parity.v
+++ Parity_checker/parity.v
@@ -5,6 +5,10 @@ module parity_generator (in, out);
     integer i;
     integer count;
 
+    //method-01
+    assign out = ^(in); // out = 1 for EVEN parity, out = 0 for ODD parity
+
+    //Method-02
     always @(*) begin
         count = 0;  
         for(i = 0; i < 4; i = i + 1) begin
