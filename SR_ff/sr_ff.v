5e2cca3bc02e35a2cb56a78eeb0ad0b4cd332e0b
Ardhish Patel <ardhishp@gmail.com>
2025-09-07 15:40:39 +0530

    Code for SR_ff written

---
+++ SR_ff/sr_ff.v
@@ -0,0 +1,18 @@
+module sr_ff (s, r, clk, rst, q);
+
+input s, r, clk, rst;
+output reg q;
+
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        q <= 1'b0;
+    end else begin
+    casex ({s,r})
+    2'b00: q <= q;
+    2'b01: q <= 1'b0;
+    2'b10: q <= 1'b1;
+    2'b11: q <= 1'bx;
+    endcase
+    end
+end
+endmodule
