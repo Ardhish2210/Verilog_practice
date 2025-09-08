13133330a89114c273d45c2abbf2b75df6633a32
Ardhish Patel <ardhishp@gmail.com>
2025-09-09 00:04:04 +0530

    comparator.v
    Corrected the logic for comparator

--- Comparator/comparator.v
+++ Comparator/comparator.v
@@ -2,25 +2,20 @@ module comparator(a, b, LT, GT, EQ);
     
 input [3:0] a, b;
 output reg LT, GT, EQ;
-integer i;
 
 always @(*) begin
-    for (i=0; i<4; i=i+1) begin
-        if (a[i] > b[i]) begin
+        if (a > b) begin
             GT = 1'b1;
             LT = 1'b0;
             EQ = 1'b0;
-        end else if (a[i] < b[i]) begin
+        end else if (a < b) begin
             GT = 1'b0;
             LT = 1'b1;
             EQ = 1'b0;
+        end else begin
+            GT = 1'b0;
+            LT = 1'b0;
+            EQ = 1'b1;
         end
     end
-  
-    if(a == b) begin
-        GT = 1'b0;
-        LT = 1'b0;
-        EQ = 1'b1;
-    end
-end
 endmodule
